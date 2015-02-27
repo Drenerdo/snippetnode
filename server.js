92c5e18cc76b6d3fb99be4cb2efce0c38ed8163d
Andre Smith <andresmith24@gmail.com>
2015-02-27 03:08:45 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,60 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var dbConfig = require('./server/db');
+var mongoose = require('mongoose');
+// Connecting to a database
+mongoose.connect(dbConfig.url);
+
+var app = express();
+
+// View engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Configuring passport
+var passport = require('passport');
+var expressSession = require('express-session');
+app.use(expressSession({secret: 'mySecretKey'}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+var flash = require('connect-flash');
+app.use(flash());
+
+// Intializing passport
+var initPassport = require('./passport/init');
+initPassport(passport);
+
+var routes = require('./routes/index')(passport);
+app.use('/', routes);
+
+// Catching 404 errors
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+if(app.get('env') === 'development') {
+  app.use(function(err, req, res, next){
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+module.exports = app;
---
+++ server/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+  // 'url' : 'mongodb://<dbuser>:<dbpassword>@ds047911.mongolab.com:47911/snippetlive-dev'
+  'url' : 'mongodb://localhost/snippetlive-dev'
+}
